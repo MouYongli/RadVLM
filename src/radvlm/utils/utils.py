3d26b11d3c7bcba3aa48b70ca8c21e515fc8aa0d
Antonia Gustke <antonia.gustke@rwth-aachen.de>
2025-04-16 11:28:50 +0200

    Update pyproject.toml

---
+++ src/radvlm/utils/utils.py
@@ -0,0 +1,11 @@
+def hello(name: str) -> str:
+    """
+    Function to greet a user by name.
+
+    Args:
+        name (str): The name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
