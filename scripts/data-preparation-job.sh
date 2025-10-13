b4a730a86bc0d98031739b2f1421cf87f0600f7e
Antonia Gustke <antonia.gustke@rwth-aachen.de>
2025-10-13 19:12:35 +0200

    feat(preprocessing): add script to transform .dcm files to .jpg

---
+++ scripts/data-preparation-job.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/zsh
+
+### Job name
+#SBATCH --job-name=Data-Preparation
+
+### Output path for stdout and stderr
+### %J is the job ID, %I is the array ID
+#SBATCH --output=output_%J.txt
+
+### Request the time you need for execution. The full format is D-HH:MM:SS
+### You must at least specify minutes OR days and hours and may add or
+### leave out any other parameters
+#SBATCH --time=01:00:00
+
+### Request a host with a GPU
+### If you need two GPUs, change the number accordingly
+#SBATCH --gres=gpu:1
+
+### Project id
+#SBATCH --account=p0025751
+
+### Partition
+#SBATCH --partition=c23g
+
+export CONDA_ENV_NAME=deepseekenv
+export CONDA_ROOT=$HOME/miniforge3
+export PATH="$CONDA_ROOT/bin:$PATH"
+
+export PROJECT_ROOT="$HOME/RadVLM"
+
+conda activate $CONDA_ENV_NAME
+echo "Project root is: $PROJECT_ROOT"
+echo "Home is: $HOME"
+echo "HPCWORK is: $HPCWORK"
+cd $PROJECT_ROOT/src/radvlm/utils
+
+python preprocess_images.py
--- src/radvlm/utils/preprocess_images.py
+++ src/radvlm/utils/preprocess_images.py
@@ -1,5 +1,82 @@
 from PIL import Image
 import os
+import pydicom
+import numpy as np
+
+def transform_dcm_to_jpg(data_dir: str) -> str:
+    """
+    Transform all DICOM images in the given directory to JPEG format.
+
+    Args:
+        data_dir (str): Path to the directory containing DICOM images.
+
+    Returns:
+        None
+    """
+    for root, dirs, files in os.walk(data_dir):
+        for file in files:
+            if file.endswith('.dcm'):
+                input_image_path = os.path.join(root, file)
+                # Check if the JPEG file already exists
+                jpeg_path = input_image_path.replace('.dcm', '.jpg')
+                if os.path.exists(jpeg_path):
+                    continue
+
+                # Read the DICOM file
+                dcm_image = pydicom.dcmread(input_image_path)
+                pixel_array = dcm_image.pixel_array.astype(np.float32)
+
+                # Apply rescale slope and intercept
+                # Rescale slope and intercept are used to convert pixel values to Hounsfield units (HU) in CT images
+                # They are not always present, so we use default values of 1 and 0 if they are not found
+                slope = getattr(dcm_image, 'RescaleSlope', 1)
+                intercept = getattr(dcm_image, 'RescaleIntercept', 0)
+                pixel_array = pixel_array * slope + intercept
+
+                # Apply windowing if available
+                # Windowing is used to enhance the contrast of the image
+                window_center = getattr(dcm_image, 'WindowCenter', None)
+                window_width = getattr(dcm_image, 'WindowWidth', None)
+                if window_center and window_width:
+                    if isinstance(window_center, pydicom.multival.MultiValue):
+                        window_center = window_center[0]
+                    if isinstance(window_width, pydicom.multival.MultiValue):
+                        window_width = window_width[0]
+                    lower = window_center - window_width / 2
+                    upper = window_center + window_width / 2
+                    pixel_array = np.clip(pixel_array, lower, upper)
+                else:
+                    lower, upper = pixel_array.min(), pixel_array.max()
+
+                # Normalize to 8-bit
+                pixel_array = ((pixel_array - lower) / (upper - lower + 1e-8) * 255).astype(np.uint8)
+                image = Image.fromarray(pixel_array, mode='L')
+                image.save(jpeg_path, 'JPEG')
+
+                # Remove the original DICOM file to save space
+                os.remove(input_image_path)
+                print(f"Converted {input_image_path} to {jpeg_path}")
+    return 
+
+
+def delete_index_files(data_dir):
+    """
+    Delete all index.html files in the given directory and its subdirectories.
+    
+    Args:
+        data_dir (str): Path to the directory to search for .idx files.
+
+    Returns:
+        None
+    """
+    for root, dirs, files in os.walk(data_dir):
+        for file in files:
+            if file == 'index.html':
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+                print(f"Deleted {file_path}")
+    return
+
 
 def resize_images(max_resolution=2048):
     """
@@ -42,6 +119,10 @@ def resize_images(max_resolution=2048):
     return
 
 if __name__ == "__main__":
-    resize_images(max_resolution=2048)
-    resize_images(max_resolution=1024)
-    print("Image resizing completed.")
+    # resize_images(max_resolution=2048)
+    # resize_images(max_resolution=1024)
+    # print("Image resizing completed.")
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "data", "raw", "MIMIC-CXR", "p15"))
+    print(data_dir)
+    delete_index_files(data_dir)
+    transform_dcm_to_jpg(data_dir)
