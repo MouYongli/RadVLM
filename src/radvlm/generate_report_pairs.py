cbfe68c22349a5ed7492668fe23f737be15c98e0
antoniagu <antonia.gustke@rwth-aachen.de>
2026-04-30 08:25:06 +0000

    refactor(report candidates)

--- src/radvlm/generate_report_pairs.py
+++ src/radvlm/generate_report_pairs.py
@@ -146,12 +146,12 @@ def generate_reports():
     print("Generating reports using pre-trained DeepSeek VL2 model...", flush=True)
     
     here = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(here, "../../results/pretraining/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-100pct-vision-final")
+    model_path = os.path.join(here, "../../results/pretraining/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-p10-p11-p12-6vision-final")
     report_generator = DeepSeekVL2ReportPairGenerator(model_path=model_path)
-    raw_data = load_dataset()
+    raw_data = load_dataset(["p18"])
     
     dpo_dataset = RadVLMDatasetDeepseek(raw_data, report_generator.processor, report_generator.tokenizer, split="train", mode="eval", sample_fraction=0.25)
-    print(dpo_dataset[0])
+    # print(dpo_dataset[0])
     
     # Custom collate function that includes all necessary fields
     def collate_fn(batch):
@@ -193,7 +193,7 @@ def generate_reports():
             ground_truth_reports.append(gt_report)
             image_paths.append(images)
 
-    output_file = os.path.join(here, "../../results/dpo_dataset/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-100pct-vision-final-generated-report-pairs-p11.txt")
+    output_file = os.path.join(here, "../../results/dpo_dataset/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-p10-p11-p12-6vision-final-generated-report-pairs-p18.txt")
 
     # Create output directory if it doesn't exist
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
@@ -208,7 +208,7 @@ def generate_reports():
             f.write("="*80 + "\n")
     
     # Also save results as JSON for easier parsing later
-    json_output_file = os.path.join(here, "../../results/dpo_dataset/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-100pct-vision-final-generated-report-pairs-p11.json")
+    json_output_file = os.path.join(here, "../../results/dpo_dataset/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-p10-p11-p12-6vision-final-generated-report-pairs-p18.json")
     print(f"\nSaving reports to {json_output_file}", flush=True) 
     if len(ground_truth_reports) != len(study_ids):
         print("Warning: Number of ground truth reports does not match number of generated reports.", flush=True) 
