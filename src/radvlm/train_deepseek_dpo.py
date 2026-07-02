2c4a958227e1327b8a6af0820c93e7f688a71fe5
Antonia Gustke <antonia.gustke@rwth-aachen.de>
2026-07-02 11:43:05 +0000

    refactor(dpo-training)

--- src/radvlm/train_deepseek_dpo.py
+++ src/radvlm/train_deepseek_dpo.py
@@ -40,9 +40,9 @@ logger = logging.getLogger(__name__)
 class TrainingConfig:
     # Paths
     
-    model_path: str = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/pretraining/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-70pctdata-allsubsets-final"
+    model_path: str = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/pretraining/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-100pctdata-allsubsets-final"
     base_model_path: str = "deepseek-ai/deepseek-vl2-small"
-    output_dir: str = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo/deepseek-vl2-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model24-10pctdataset-frommodel23-5e-1lambda"
+    output_dir: str = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo/deepseek-vl2-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model27-10pctdataset-frommodel23-1e-2lambda-discrete"
 
     # Training
     num_train_epochs:            int   = 3
@@ -73,7 +73,7 @@ class TrainingConfig:
 
     # W&B
     wandb_project: str = "deepseek-vl2-mimic-cxr-dpo"
-    wandb_run:     str = "deepseek-vl2-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model24-10pctdataset-frommodel23-5e-1lambda"
+    wandb_run:     str = "deepseek-vl2-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model27-10pctdataset-frommodel23-1e-2lambda-discrete"
 
 
 def parse_args() -> TrainingConfig:
@@ -633,7 +633,7 @@ def main():
 
     # Data
     logger.info("Loading preference dataset ...")
-    preference_data = load_preference_dataset("/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-5e-1lambda.json")
+    preference_data = load_preference_dataset("/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-1e-2lambda-discrete.json")
 
     print(f"Loaded perference dataset with {len(preference_data)} items")
     dataset_wrapper = RadVLMDPODataset(
