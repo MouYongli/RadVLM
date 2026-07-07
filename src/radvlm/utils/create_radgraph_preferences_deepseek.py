1bddb8fa3a829b02cdc7e1eb1420c64de1e34723
Antonia Gustke <antonia.gustke@rwth-aachen.de>
2026-07-07 17:22:32 +0000

    refactor(dpo-dataset): correct sampling

--- src/radvlm/utils/create_radgraph_preferences_deepseek.py
+++ src/radvlm/utils/create_radgraph_preferences_deepseek.py
@@ -26,8 +26,18 @@ def run_batched_radgraph(scorer, hyps, refs, batch_size):
 
 
 def build_preference(item, meteor_1, meteor_2, rg1, rg2, lam):
-    reward_1 = lam * meteor_1 + (1 - lam) * rg1[2]  # rg[2] = complete score
-    reward_2 = lam * meteor_2 + (1 - lam) * rg2[2]
+    if meteor_1 >= meteor_2:
+        m1_discrete = 1
+        m2_discrete = 0
+    else:
+        m1_discrete = 0
+        m2_discrete = 1
+        
+ 
+    reward_1 = lam * m1_discrete + (1 - lam) * rg1[2]
+    reward_2 = lam * m2_discrete + (1 - lam) * rg2[2]
+    # reward_1 = lam * meteor_1 + (1 - lam) * rg1[2]  # rg[2] = complete score
+    # reward_2 = lam * meteor_2 + (1 - lam) * rg2[2]
     return {
         "study_id": item['study_id'],
         "image_paths": item['image_paths'],
@@ -86,6 +96,14 @@ def create_radgraph_preferences_deepseek(preference_dataset_path, output_path):
 
     # remove items, where item[radgraph_complete_report_1]==0 and item[radgraph_complete_report_2]==0
     preferences = [item for item in preferences if item["radgraph_complete_report_1"] != 0 or item["radgraph_complete_report_2"] != 0]
+    preferences = [
+        data for data in preferences
+        if not (
+            (data["radiologist_preference"] == "report_1" and data["radgraph_complete_report_1"] == 0)
+            or
+            (data["radiologist_preference"] == "report_2" and data["radgraph_complete_report_2"] == 0)
+        )
+    ]
     
     with open(output_path, 'w') as f:
         json.dump(preferences, f, indent=4)
@@ -93,6 +111,6 @@ def create_radgraph_preferences_deepseek(preference_dataset_path, output_path):
     print(f"Saved {len(preferences)} preferences to {output_path}.")
     
 if __name__ == "__main__":
-    preference_dataset_path = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-60pctdata-allsubsets-final-generated-report-pairs-10pctdataset.json"
-    output_path = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-1e-2lambda.json"
+    preference_dataset_path = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/deepseek-vl2-mimic-cxr-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-60pctdata-allsubsets-final-generated-report-pairs-10pctdataset-correct-sampling.json"
+    output_path = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-1e-2lambda-discrete-processed-corrected-sampling.json"
     create_radgraph_preferences_deepseek(preference_dataset_path, output_path)
--- src/radvlm/utils/recompute_preferences.py
+++ src/radvlm/utils/recompute_preferences.py
@@ -53,8 +53,8 @@ def recompute_item(item: dict, lam: float) -> dict:
  
 def main():
  
-    input_path  = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-5e-1lambda-discrete.json"
-    output_path = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-5e-1lambda-discrete-processed.json"
+    input_path  = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-5pctdatasetreports-1e-2lambda-discrete-processed-corrected-sampling.json"
+    output_path = "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-5pctdatasetreports-5e-1lambda-discrete-processed-corrected-sampling.json"
     lam         = 0.5
  
     if not 0.0 <= lam <= 1.0:
