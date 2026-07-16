174a07835f70756102fa3bf05e5d56f986450317
Antonia Gustke <antonia.gustke@rwth-aachen.de>
2026-07-16 15:40:45 +0000

    refactor(dpo-training)

--- src/radvlm/train_medgemma_dpo.py
+++ src/radvlm/train_medgemma_dpo.py
@@ -59,7 +59,7 @@ class TrainingConfig:
         "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/pretraining/medgemma-1.5-mimic-cxr-poc-lora-r8-lr1e-4-3epochs-cosine-5pctwarmup-6earlystop-100pctdata-final"
     )
     output_dir: str = (
-        "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo/medgemma-1.5-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model15-deepseek-dataset-large-5e-1lambda"
+        "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo/medgemma-1.5-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model15-deepseek-dataset-large-1e-2lambda"
     )
 
     # Training
@@ -92,7 +92,7 @@ class TrainingConfig:
     # W&B
     wandb_project: str = "medgemma-1.5-mimic-cxr-dpo"
     wandb_run:     str = (
-        "medgemma-1.5-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model15-deepseek-dataset-large-5e-1lambda"
+        "medgemma-1.5-mimic-cxr-dpo-lora-r16-lr5e-5-beta0.1-model15-deepseek-dataset-large-1e-2lambda"
     )
 
 
@@ -544,7 +544,7 @@ def main():
     # ── Data ──────────────────────────────────────────────────────────────
     logger.info("Loading preference dataset ...")
     preference_data = load_preference_dataset(
-        "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-5e-1lambda-discrete-processed-corrected-sampling.json"
+        "/pfss/mlde/workspaces/mlde_wsp_RWTH_MedReport/ag88juba/RadVLM/results/dpo_dataset/model23-10pctdatasetreports-1e-2lambda-discrete-processed-corrected-sampling.json"
     )
     dataset_wrapper = RadVLMDPODatasetMedGemma(
         preference_data, processor, processor.tokenizer,
